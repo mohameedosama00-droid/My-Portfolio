82737f4e611cda00781b3d68bda1c3da57dd8dbf
Mohamed Osama <mohameedosama00@gmail.com>
2026-02-04 22:52:04 +0200

    Implement typing effect and enhance scroll reveal
    Added typing effect for developer roles and improved scroll reveal functionality.
    
    Signed-off-by: Mohamed Osama <mohameedosama00@gmail.com>

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ window.addEventListener("scroll", () => {
 })
 
 document.getElementById("profile").onclick = function() {this.classList.toggle("zoomed");};
+const texts = ["Frontend Developer", "JavaScript Developer", "React Developer (soon)"];
+let index = 0;
+let charIndex = 0;
+const speed = 100;
+const typingText = document.getElementById("typing-text");
+function type() {
+  if (charIndex < texts[index].length) {
+    typingText.textContent += texts[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, speed);
+  } else {
+    setTimeout(erase, 1500);
+  }}
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = texts[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 60);
+  } else {
+    index = (index + 1) % texts.length;
+    setTimeout(type, 300);
+  }
+}
+type();
+    
+
     
